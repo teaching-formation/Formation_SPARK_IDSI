16e835bf7536de867c3ff4b51320999d1de28f00
Thindy7 <nhourakeith@gmail.com>
2022-06-11 14:12:46 +0000

    my first job

--- src/main/scala/jobs/Formation1.scala
+++ src/main/scala/jobs/Formation1.scala
@@ -1,8 +1,16 @@
 package jobs
 
 import org.apache.spark.sql.SparkSession
+// import com.typesafe.config.ConfigFactory
+
+import org.slf4j.LoggerFactory
+import org.apache.log4j.Level
 
 object Formation1 extends App {
+
+  val _LOGGER = LoggerFactory.getLogger(classOf[App])
+  org.apache.log4j.Logger.getLogger("org").setLevel(Level.OFF)
+  org.apache.log4j.Logger.getLogger("akka").setLevel(Level.OFF)
 
   val spark = SparkSession
     .builder()
@@ -10,5 +18,25 @@ object Formation1 extends App {
     .enableHiveSupport()
     .getOrCreate()
 
+  val dataSeq = Seq(("Java", 20000), ("Python", 100000), ("Scala", 3000))
 
+  val rdd=spark.sparkContext.parallelize(dataSeq)
+
+  println("Nombre initial de partitions :"+ rdd.getNumPartitions)
+  println("Collect :"+ rdd.collect() )
+  println("Premier element :"+ rdd.first())
+
+  val rdd2 = rdd.map(f => { (f._2, (f._1,f._2))} )
+
+  val rdd3 = rdd.sortByKey(false, 6)
+
+  val rdd4 = rdd.reduce( (a,b)=> ("min",a._2 min b._2))._2
+
+  val data = Seq(("Project", 1), ("Gutenberg’s", 1), ("Alice’s", 1), ("Adventures", 1), ("in", 1), ("Wonderland", 1), ("Project", 1), ("Gutenberg’s", 1), ("Adventures", 1), ("in", 1), ("Wonderland", 1), ("Project", 1), ("Gutenberg’s", 1))
+
+  val rdd5 = spark.sparkContext.parallelize(data)
+
+  val rdd6 = rdd5.reduceByKey(_ + _)
+
+  rdd6.foreach(println)
 }
