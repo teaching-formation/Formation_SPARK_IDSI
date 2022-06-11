2a15a060260b66ff3ec4f765948fd18e0a5852e2
Macbook <macbook@lynx.local>
2022-06-11 15:58:44 +0000

    my first job

--- src/main/scala/jobs/Formation1.scala
+++ src/main/scala/jobs/Formation1.scala
@@ -1,14 +1,45 @@
 package jobs
 
 import org.apache.spark.sql.SparkSession
+import org.slf4j.LoggerFactory
+import org.apache.log4j.Appender
+import org.apache.log4j.Level
+
 
 object Formation1 extends App {
 
+  val _LOGGER = LoggerFactory.getLogger(classOf[App])
+  org.apache.log4j.Logger.getLogger("org").setLevel(Level.OFF)
+  org.apache.log4j.Logger.getLogger("akka").setLevel(Level.OFF)
+
+
   val spark = SparkSession
     .builder()
-    .appName("Fisrt Job SPARK")
+    .appName("First Job SPARK")
     .enableHiveSupport()
     .getOrCreate()
+
+  val dataSeq = Seq(("java",20000), ("python",100000), ("scala",20000))
+  val rdd=spark.sparkContext.parallelize(dataSeq)
+  println("Number initial de partitions :"+ rdd.getNumPartitions)
+
+  println("collect :"+ rdd.collect())
+
+  println("Premier element :"+ rdd.first())
+
+  val rdd3 = rdd.sortByKey(ascending = false,numPartitions = 6)
+
+  val rdd4 = rdd.reduce( (a,b)=> ("min", a._2 min b._2))._2
+
+  val data = Seq(("Project",1),
+    ("Gutenberg's",1),
+    ("Alice's",1),
+    ("Adventures",1),
+    ("in",1),("Wonderland",1),
+    ("Project",1),
+    ("Gutenberg's",1))
+
+
 
 
 }
