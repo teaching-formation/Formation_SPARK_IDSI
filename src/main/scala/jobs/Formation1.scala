2b085b774332eacf359dd8d42e27c83888dac138
KZAS <zagvil1@gmail.com>
2022-06-11 05:32:25 +0000

    "Mon premier commit"

--- src/main/scala/jobs/Formation1.scala
+++ src/main/scala/jobs/Formation1.scala
@@ -10,5 +10,39 @@ object Formation1 extends App {
     .enableHiveSupport()
     .getOrCreate()
 
+  val dataSeq = Seq(("Java", "20000"), ("Python", "100000"), ("Scala", "3000"))
 
+  val rdd = spark.sparkContext.parallelize(dataSeq)
+
+  println("Nombre initial de partitions :"+ rdd.getNumPartitions)
+
+  println("Collect :"+ rdd.collect() )
+
+  println("Premier element :"+ rdd.first())
+
+  val rdd2 = rdd.map(f => { (f._2, (f._1,f._2))} )
+
+  val rdd3 = rdd.sortByKey(false, 6)
+
+  val rdd4 = rdd.reduce( (a,b)=> ("min",a._2 min b._2))._2
+
+  val data = Seq(("Project", 1),
+    ("Gutenberg’s", 1),
+    ("Alice’s", 1),
+    ("Adventures", 1),
+    ("in", 1),
+    ("Wonderland", 1),
+    ("Project", 1),
+    ("Gutenberg’s", 1),
+    ("Adventures", 1),
+    ("in", 1),
+    ("Wonderland", 1),
+    ("Project", 1),
+    ("Gutenberg’s", 1))
+
+  val rdd5=spark.sparkContext.parallelize(data)
+
+  val rdd6=rdd.reduceByKey(_ + _)
+
+  rdd6.foreach(println)
 }
